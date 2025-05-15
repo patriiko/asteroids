ba892ee05ff7c3761560b1286ac8004bbfacbf79
patriiko <ostrunicpersona@gmail.com>
2025-05-15 11:09:33 +0200

    add asteroids to the screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,10 +12,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -23,8 +29,10 @@ def main():
         updatable.update(dt)
 
         screen.fill((0, 0, 0))
+
         for thing in drawable:
             thing.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
